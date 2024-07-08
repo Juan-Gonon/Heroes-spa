04eeba96962d0e88baa829bea5c9285ab24983b0
Juan Gonon <juanfegonon27@gmail.com>
2024-07-08 15:26:57 -0600

    navbar

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -2,10 +2,12 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 import { MarvelPage } from '../heroes/pages/MarvelPage'
 import { DcPage } from '../heroes/pages/DcPage'
 import { LoginPage } from '../auth/pages/LoginPage'
+import { Navbar } from '../ui'
 
 export function MyRoutes () {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path='/marvel' element={<MarvelPage />} />
         <Route path='/dc' element={<DcPage />} />
