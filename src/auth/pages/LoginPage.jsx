c7e65bb169c7dfcdc6b02d4411221b8af9e4f1da
Juan Gonon <juanfegonon27@gmail.com>
2024-07-09 09:46:28 -0600

    login

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,5 +1,19 @@
+import { useNavigate } from 'react-router-dom'
+
 export function LoginPage () {
+  const navigate = useNavigate()
+
+  const handleClickLogin = () => {
+    navigate('/', {
+      replace: true
+    })
+  }
+
   return (
-    <div>LoginPage</div>
+    <div className='container mt-5'>
+      <h1>Login</h1>
+      <hr />
+      <button className='btn btn-primary' onClick={handleClickLogin}>Login</button>
+    </div>
   )
 }
