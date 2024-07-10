b69dd89eb8b2f3efe196a6f2d6af1e1eeadc06fa
Juan Gonon <juanfegonon27@gmail.com>
2024-07-10 09:54:12 -0600

    query params and query string

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,6 +1,26 @@
 // import { HeroCard } from "../components"
 
+import { useLocation, useNavigate } from 'react-router-dom'
+import { useForm } from '../../hooks/useForm'
+import queryString from 'query-string'
+
 export function SearchPage () {
+  const { searchText, onInputChange } = useForm({
+    searchText: ''
+  })
+
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  const { q = '' } = queryString.parse(location.search)
+
+  const handleSubmitForm = (e) => {
+    e.preventDefault()
+    if (searchText.trim().length < 1) return
+    console.log(searchText)
+    navigate(`?q=${searchText}`)
+  }
+
   return (
     <>
       <h1>Search</h1>
@@ -10,8 +30,16 @@ export function SearchPage () {
         <div className='col-5'>
           <h4>Searching</h4>
           <hr />
-          <form action=''>
-            <input type='text' placeholder='Search a hero' className='form-control' name='searchText' autoComplete='off' />
+          <form onSubmit={handleSubmitForm}>
+            <input
+              type='text'
+              placeholder='Search a hero'
+              className='form-control'
+              name='searchText'
+              autoComplete='off'
+              value={searchText}
+              onChange={onInputChange}
+            />
             <button className='btn btn-outline-primary mt-1 '>Search</button>
           </form>
         </div>
@@ -22,7 +50,7 @@ export function SearchPage () {
             Search a hero
           </div>
           <div className='alert alert-danger'>
-            No hero With <b>ABC</b>
+            No hero With <b>{q}</b>
           </div>
           {/* <HeroCard /> */}
 
