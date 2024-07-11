1bbd5fa3e0d287d9f5e05b4fabeac1d42624f7c9
Juan Gonon <juanfegonon27@gmail.com>
2024-07-11 14:06:47 -0600

    initilizador useReducer

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -7,19 +7,30 @@ const initialState = {
   logged: false
 }
 
+const init = () => {
+  const user = JSON.parse(localStorage.getItem('user'))
+
+  return {
+    logged: !!user,
+    user
+  }
+}
+
 // eslint-disable-next-line react/prop-types
 export function AuthProvider ({ children }) {
-  const [authState, dispatch] = useReducer(authReducer, initialState)
+  const [authState, dispatch] = useReducer(authReducer, initialState, init)
 
   const login = (name = '') => {
-    const action = {
-      type: types.login,
-      payload: {
-        id: 'ABC',
-        name
-      }
+    const user = {
+      id: 'ABC',
+      name
     }
 
+    const action = {
+      type: types.login,
+      payload: user
+    }
+    localStorage.setItem('user', JSON.stringify(user))
     dispatch(action)
   }
 
