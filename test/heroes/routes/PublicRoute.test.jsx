6783932b3ef84fa1fe0356444b2f703cd707f102
Juan Gonon <juanfegonon27@gmail.com>
2024-07-19 11:38:50 -0600

    navigate routes

--- test/heroes/routes/PublicRoute.test.jsx
+++ test/heroes/routes/PublicRoute.test.jsx
@@ -2,6 +2,7 @@ import { render, screen } from '@testing-library/react'
 import { describe, expect, it, vi } from 'vitest'
 import { PublicRoute } from '../../../src/heroes/routes/PublicRoute'
 import { useAuth } from '../../../src/hooks/useAuth'
+import { MemoryRouter, Routes, Route } from 'react-router-dom'
 
 vi.mock('../../../src/hooks/useAuth')
 
@@ -22,6 +23,33 @@ describe('Pruebas en <PublicRoute /> ', () => {
   })
 
   it('debe de navegar si está autenticado ', () => {
+    useAuth.mockReturnValue({
+      logged: true
+    })
 
+    render(
+      <MemoryRouter initialEntries={['/login']}>
+
+        <Routes>
+          <Route
+            path='/marvel' element={
+              <h1>Pagina Marvel</h1>
+            }
+          />
+
+          <Route
+            path='/login' element={
+              <PublicRoute>
+                <h1>Ruta Pública</h1>
+              </PublicRoute>
+          }
+          />
+        </Routes>
+
+      </MemoryRouter>
+    )
+
+    // screen.debug()
+    expect(screen.getByText('Pagina Marvel')).toBeTruthy()
   })
 })
