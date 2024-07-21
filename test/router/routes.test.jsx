2e754e78064bf5ab92cdf753dafed9d657dab8d3
Juan Gonon <juanfegonon27@gmail.com>
2024-07-20 23:31:42 -0600

    /marvel

--- test/router/routes.test.jsx
+++ test/router/routes.test.jsx
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react'
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it, vi } from 'vitest'
 import { AuthContext } from '../../src/auth'
 import { MemoryRouter } from 'react-router-dom'
 import { MyRoutes } from '../../src/router/routes'
+import { useAuth } from '../../src/hooks/useAuth'
+
+vi.mock('../../src/hooks/useAuth')
 
 describe('Pruebas en <MyRoutes />', () => {
   it('debe de mostrar el login si no está autenticado ', () => {
@@ -20,5 +23,29 @@ describe('Pruebas en <MyRoutes />', () => {
 
     // screen.debug()
     expect(screen.getAllByText('Login').length).toBe(2)
+  })
+
+  it('debe de mostrar el componente de Marvel si está autenticado ', () => {
+    useAuth.mockReturnValue({
+      logged: true
+    })
+    // const contextValue = {
+    //   logged: true,
+    //   user: {
+    //     id: 'ABC',
+    //     name: 'Juan'
+    //   }
+    // }
+    render(
+      <MemoryRouter initialEntries={['/login']}>
+        {/* <AuthContext.Provider value={contextValue}> */}
+        <MyRoutes />
+        {/* </AuthContext.Provider> */}
+
+      </MemoryRouter>
+    )
+
+    // screen.debug()
+    expect(screen.getAllByText('Marvel').length).toBeGreaterThanOrEqual(1)
   })
 })
