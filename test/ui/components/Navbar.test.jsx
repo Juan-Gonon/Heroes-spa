c435b1fa5e685c1c5c55b024bc9d91ecb604a41d
Juan Gonon <juanfegonon27@gmail.com>
2024-07-21 12:26:50 -0600

    llamar logout

--- test/ui/components/Navbar.test.jsx
+++ test/ui/components/Navbar.test.jsx
@@ -1,5 +1,5 @@
-import { render, screen } from '@testing-library/react'
-import { describe, expect, it, vi } from 'vitest'
+import { cleanup, fireEvent, render, screen } from '@testing-library/react'
+import { afterEach, describe, expect, it, vi } from 'vitest'
 import { Navbar } from '../../../src/ui/components/Navbar'
 import { useAuth } from '../../../src/hooks/useAuth'
 import { MemoryRouter } from 'react-router-dom'
@@ -7,13 +7,19 @@ import { MemoryRouter } from 'react-router-dom'
 vi.mock('../../../src/hooks/useAuth')
 
 describe('Pruebas en <Navbar /> ', () => {
+  const logout = vi.fn()
+  afterEach(() => {
+    cleanup()
+    vi.clearAllMocks()
+  })
+  useAuth.mockReturnValue({
+    logout,
+    user: {
+      name: 'Juan'
+    }
+  })
+
   it('debe demostrar el nombre del usuario', () => {
-    useAuth.mockReturnValue({
-      logged: true,
-      user: {
-        name: 'Juan'
-      }
-    })
     render(
       <MemoryRouter>
         <Navbar />
@@ -26,6 +32,16 @@ describe('Pruebas en <Navbar /> ', () => {
   })
 
   it('debe de llamar el logout y  navigate cuando se hace click en el botón ', () => {
+    render(
+      <MemoryRouter>
+        <Navbar />
+      </MemoryRouter>
+    )
 
+    // screen.debug()
+    const button = screen.getByRole('button', { name: 'btn-logout' })
+
+    fireEvent.click(button)
+    expect(logout).toBeCalled()
   })
 })
