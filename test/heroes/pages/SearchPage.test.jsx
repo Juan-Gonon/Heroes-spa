518525b35f6ab27b454808461ba8e8ac75bff019
Juan Gonon <juanfegonon27@gmail.com>
2024-07-21 13:55:14 -0600

    snapshots SearchPage

---
+++ test/heroes/pages/SearchPage.test.jsx
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+import { describe, expect, it } from 'vitest'
+import { SearchPage } from '../../../src/heroes/pages/SearchPage'
+
+describe('Pruebas en <SearchPage /> ', () => {
+  it('debe de mostrarse correctamente con valores por defecto ', () => {
+    const { container } = render(
+      <MemoryRouter>
+        <SearchPage />
+      </MemoryRouter>
+    )
+
+    expect(container).toMatchSnapshot()
+  })
+})
